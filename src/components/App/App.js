604cde1780e7f561aaaea6e5870a2a57d7c86833
Natasha <natasha.chinea@gmail.com>
2022-11-22 10:06:35 +0000

    Folders&files

---
+++ src/components/App/App.js
@@ -0,0 +1,19 @@
+import './App.css';
+import CreateCard from '../CreateCard/CreateCard';
+
+function App() {
+  return (
+    <div>
+    <CreateCard/>
+
+
+    
+    </div>
+  );
+}
+
+export default App;
+
+//plan:
+//1.design object with data (for now)
+//2.
---
+++ src/components/CreateCard/CreateCard.js
@@ -0,0 +1,15 @@
+
+function CreateCard () {
+    return (
+        <div>
+            <input placeholder='Week'></input>
+            <input placeholder='Day'></input>
+            <input placeholder='Topic'></input>
+            <img src=''/>
+
+        </div>
+
+    );
+}
+
+export default CreateCard; 
