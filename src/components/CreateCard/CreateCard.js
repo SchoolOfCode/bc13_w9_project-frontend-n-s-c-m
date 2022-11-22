409ac4995243063b41923c7a9bfe8c8c5b90281c
Sunbearian <stigcollins@icloud.com>
2022-11-22 12:39:56 +0000

    Trying to capture the topic inputs

--- src/components/CreateCard/CreateCard.js
+++ src/components/CreateCard/CreateCard.js
@@ -1,36 +1,37 @@
-import {useState} from "react"
+import { useState } from "react";
 
-function CreateCard ({cards, setCards}) {
-    const [cardInput, setCardInput] = useState()
-    function handleChangeWeek (e) {
-        console.log(e.target.value)
-        let weekInput = Number(e.target.value)
-        setCardInput({...cardInput, week: weekInput})
-    }
-    function handleChangeDay (e) {
-        console.log(e.target.value)
-        let dayInput = (e.target.value)
-        setCardInput({...cardInput, day: dayInput})
-    }
-    function handleChangeTopic (e) {
-        console.log(e.target.value)
-        let dayInput = (e.target.value)
-        setCardInput({...cardInput, topic: [topicInput]})
-    }
+function CreateCard({ cards, setCards }) {
+	const [cardInput, setCardInput] = useState();
 
-    return (
-        <div>
-            <input placeholder='Week No.' onChange={handleChangeWeek} type="number"></input>
-            <input placeholder='Day' onChange={handleChangeDay} ></input>
-            <input placeholder='Topic 1'></input>
-            <input placeholder='Topic 2'></input>
-            <input placeholder='Topic 3'></input>
-            <img src=''/>
-            <button>Click to save</button>
+	function handleChangeWeek(e) {
+		console.log(e.target.value);
+		let weekInput = Number(e.target.value);
+		setCardInput({ ...cardInput, week: weekInput });
+	}
+	function handleChangeDay(e) {
+		console.log(e.target.value);
+		let dayInput = e.target.value;
+		setCardInput({ ...cardInput, day: dayInput });
+	}
+	function handleChangeTopic1(e) {
+		let topicInput = e.target.value;
+	}
 
-        </div>
-
-    );
+	return (
+		<div>
+			<input
+				placeholder="Week No."
+				onChange={handleChangeWeek}
+				type="number"
+			></input>
+			<input placeholder="Day" onChange={handleChangeDay}></input>
+			<input placeholder="Topic 1" onChange={handleChangeTopic1}></input>
+			<input placeholder="Topic 2"></input>
+			<input placeholder="Topic 3"></input>
+			<img src="" />
+			<button>Click to save</button>
+		</div>
+	);
 }
 
-export default CreateCard; 
+export default CreateCard;
