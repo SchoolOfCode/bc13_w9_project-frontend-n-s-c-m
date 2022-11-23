d895f264daab524912b2eb2168ea4304383e20a3
Natasha <natasha.chinea@gmail.com>
2022-11-23 10:58:24 +0000

    ccs added

--- src/components/CreateCard/CreateCard.js
+++ src/components/CreateCard/CreateCard.js
@@ -72,6 +72,7 @@ function CreateCard({ cards, setCards }) {
 
 	return (
 		<div className = "createCard">
+			<h3>Enter resource details</h3>
 			<input
 				placeholder="Week No."
 				onChange={handleChangeWeek}
@@ -85,7 +86,7 @@ function CreateCard({ cards, setCards }) {
 			<input placeholder="Reference 2" onChange={handleChangeReference2}></input>
             <input placeholder="Reference 3" onChange={handleChangeReference3}></input>
 			<img src=""/>
-			<button onClick={handleClick} >Click to save</button>
+			<button className="button" onClick={handleClick} >Click to save</button>
 		</div>
 		);
 }
