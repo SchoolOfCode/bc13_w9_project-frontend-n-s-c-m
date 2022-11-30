66b325fe19b7d58aa884ade2e1527b0474262b32
Natasha <natasha.chinea@gmail.com>
2022-11-30 12:32:14 +0000

    adding react testing library

---
+++ src/components/CreateCard/CreateCard.test.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import userEvent from "@testing-library/user-event";
+import { test, expect } from "@jest/globals";
+import CreateCard from "./CreateCard.js";
+
+test("should contain input", () => {
+    render(<)
+})
+
