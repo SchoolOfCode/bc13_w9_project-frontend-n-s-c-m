a38fd337abf596cabe051a2c1a1f6cf7d725a09c
Natasha <natasha.chinea@gmail.com>
2022-11-30 14:17:50 +0000

    react library

--- src/components/CreateCard/CreateCard.test.js
+++ src/components/CreateCard/CreateCard.test.js
@@ -4,7 +4,19 @@ import userEvent from "@testing-library/user-event";
 import { test, expect } from "@jest/globals";
 import CreateCard from "./CreateCard.js";
 
+const dummieData = {
+	week: 1,
+	day: "Test day",
+	image: "http test image address",
+	topic: ["Array", "of", "tests"],
+	ref: ["Array", "of", "tests"]
+}
+
 test("should contain input", () => {
-    render(<)
+    render(<CreateCard cards={dummieData} setCards={ jest.fn()}/>)
+    const dayInput = screen.getByPlaceholderText("Day")
+    //fireEvent.change(dayInput, {target:{value:"Monday"}})
+    userEvent.type(dayInput, "Monday")
+    expect(dayInput.value).toBe("Monday"); 
 })
 
